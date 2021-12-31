b254e3cb9e46c738b3dfc3a47c24acb2817e197f
K-Roon <66551757+K-Roon@users.noreply.github.com>
2021-12-31 11:01:04 +0900

    Update signin.dart

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:chat_app/helper/helperfunctions.dart';
-import 'package:chat_app/helper/theme.dart';
+import 'package:chat_app/widgets/theme.dart';
 import 'package:chat_app/services/auth.dart';
 import 'package:chat_app/services/database.dart';
 import 'package:chat_app/views/chat_rooms_screen.dart';
